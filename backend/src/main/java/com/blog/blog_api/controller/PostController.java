5e31868906ed18cb8e15048c506f93d3e4c27fed
cesarmilan16 <cesar3bfermi@gmail.com>
2025-12-17 01:54:21 +0100

    Actualiza puertos en PostController, docker-compose y entorno de Angular

--- backend/src/main/java/com/blog/blog_api/controller/PostController.java
+++ backend/src/main/java/com/blog/blog_api/controller/PostController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4201")
 public class PostController {
 
     @Autowired
